11c1f8e5d8dcc54164e7a5e8f1ce109daa0eebcb
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-09-24 22:31:04 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/fonts'],
+  css: ['~/assets/css/main.css'],
+
+  // *Eslint config
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+
+  // *Vite config
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  // *Fonts config
+  fonts: {
+    families: [
+      // Google Fonts
+      { name: 'Montserrat', provider: 'google' },
+    ],
+    defaults: {
+      weights: [400, 600, 700], // Tải các weight mặc định
+      subsets: ['latin'], // Subset mặc định
+    },
+  },
+})
