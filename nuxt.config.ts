04cfe18ef023eb1fa483a956b6e5c791e77c508d
Lê Thiên Trí <thientrile2003@gmail.com>
2025-09-25 11:29:23 +0700

    chore: update package dependencies and add hello API endpoint
    - Updated @nuxt/ui to use a caret version for better compatibility.
    - Added @tailwindcss/postcss as a new dependency.
    - Created a new API endpoint at /api/hello that returns a simple JSON response.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/fonts'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+  ],
   css: ['~/assets/css/main.css'],
 
   // *Eslint config
@@ -14,9 +17,11 @@ export default defineNuxtConfig({
     },
   },
 
-  // *Vite config
-  vite: {
-    plugins: [tailwindcss()],
+  // *PostCSS config
+  postcss: {
+    plugins: {
+      '@tailwindcss/postcss': {},
+    },
   },
 
   // *Fonts config
